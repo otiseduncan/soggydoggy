08f6f9612274e6246058f13da9df5369bee27647
otiseduncan <otiseduncan@gmail.com>
2026-03-14 09:08:52 -0400

    Update Vite config with explicit build settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/soggydoggy/'
+  base: '/soggydoggy/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
